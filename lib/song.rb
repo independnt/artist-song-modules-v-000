e35cf6b8852b3447e787120be5b0f28514b860b8
Dennis Gonzalez <gonzalezdennis6@gmail.com>
2017-12-22 15:20:06 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -4,8 +4,8 @@ class Song
   attr_accessor :name
   attr_reader :artist
 
-  include Paramable
-  extend Memorable
+  include Memorable::InstanceMethods
+  extend Memorable::ClassMethods
   extend Findable
   @@songs = []
 
